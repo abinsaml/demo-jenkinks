29a36cd3259a1aace2b033b8d3af1a6810dc2166
abinsaml <samlabinash@gmail.com>
2019-10-29 16:06:48 +0530

    Should fail

--- src/main/java/com/jenkins/example/demojenkinks/controller/HelloController.java
+++ src/main/java/com/jenkins/example/demojenkinks/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.jenkins.example.demojenkinks.controller;
 
+import com.jenkins.example.demojenkinks.DemoJenkinksApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    public static Logger logger= LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
     public String hello(){
         return "Hello From Jenkins";
